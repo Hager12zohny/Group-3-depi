479f5b4d7a18fcfd9a16d1389ac495871f4259c0
movieapp <hagarrallaa@gmail.com>
2025-10-12 23:24:07 +0300

    uppdate

--- app/src/main/java/com/example/movieapplication/screens/HomeScreen.kt
+++ app/src/main/java/com/example/movieapplication/screens/HomeScreen.kt
@@ -1,8 +1,5 @@
 package com.example.movieapplication.screens
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -29,15 +26,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.movieapplication.R
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MovieHomeScreen()
-        }
-    }
-}
 
 @Composable
 fun MovieHomeScreen() {
