ba10c25c39c06e6e345e19b0846a24f72eade0f8
habibamagdy142 <habibamagdy82@gmail.com>
2025-10-13 16:08:44 +0300

    Add Movie data class

---
+++ app/src/main/java/com/example/movieapplication/model/Movie.kt
@@ -0,0 +1,10 @@
+package com.example.movieapplication.model
+
+data class Movie(
+    val id: Int,
+    val title: String?,
+    val poster_path: String?,
+    val vote_average: Double?,
+    val release_date: String?,
+    val overview: String?
+)
